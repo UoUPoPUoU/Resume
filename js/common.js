bbf41b741de697c880f263003eca0b763a919634
825086234@qq.com <uoupopuou>
2018-05-06 23:39:59 +0800

    code align

--- js/common.js
+++ js/common.js
@@ -5,33 +5,30 @@
         btn = document.getElementsByClassName("nav-btn")[0],
         body = document.body,
         navBox = document.getElementsByClassName("nav-box")[0];
-    for (var i = 0; i < navLi.length; i++) {
-      navLi[i].index = i;
-      navLi[i].onclick = function () {
-        for (var j = 0; j < tabDiv.length; j++) {
-            tabDiv[j].classList.remove("act");
-            navLi[j].classList.remove("act");
+
+        for (var i = 0; i < navLi.length; i++) {
+          navLi[i].index = i;
+          navLi[i].onclick = function () {
+            for (var j = 0; j < tabDiv.length; j++) {
+                tabDiv[j].classList.remove("act");
+                navLi[j].classList.remove("act");
+            }
+           
+            tabDiv[this.index].classList.add("act");
+            navLi[this.index].classList.add("act");
+          }
         }
-       
-        tabDiv[this.index].classList.add("act");
-        navLi[this.index].classList.add("act");
-      }
-  }
-  btn.onclick = function () {
-    changeAct();
-    }
-  navBox.onclick = function () {
-    changeAct();
-    }
-
-  function changeAct(){
-    btn.classList.toggle("on");
-       navBox.classList.toggle("act");
-       if(screen.width < 767){
-        body.classList.toggle("act");
-       }
-    }
-
-
-
+        btn.onclick = function () {
+          changeAct();
+          }
+        navBox.onclick = function () {
+          changeAct();
+          }
+        function changeAct(){
+          btn.classList.toggle("on");
+             navBox.classList.toggle("act");
+             if(screen.width < 767){
+              body.classList.toggle("act");
+             }
+          }
 })(); 
